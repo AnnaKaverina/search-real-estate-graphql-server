e67f731a00261227c6da5466fadf6c79c0c169b6
Anna <aik303030@gmail.com>
2023-12-09 21:48:21 +0300

    feat: changing typeDefs

--- index.js
+++ index.js
@@ -1,33 +1,58 @@
-const todosUrl = 'https://jsonplaceholder.typicode.com/todos';
+// const todosUrl = 'https://jsonplaceholder.typicode.com/todos';
 import { ApolloServer, gql } from 'apollo-server';
-import fetch from 'node-fetch';
+// import fetch from 'node-fetch';
+import stubData from './stub/realEstateObjects.js'
 
 const typeDefs = gql`
-    type Todo {
-        userId: Int
-        id: Int
-        title: String
-        completed: Boolean
+    type House {
+        id: ID!
+        square: Int
     }
-    
+
+    type LandPlot {
+        id: ID!
+        square: Int
+    }
+
+    type RealEstateObject {
+        id: ID!
+        type: String!
+        cost: Int
+        landPlot: LandPlot!
+        house: House
+    }
+
+    input Filter {
+        minLandPlotSquare: Int
+        maxLandPlotSquare: Int
+        minHouseSquare: Int
+        maxHouseSquare: Int
+        minCost: Int
+        maxCost: Int
+    }
+
     type Query {
-        todos(limit: Int! = 5, start: Int! = 0): [Todo]
-        todo(id: String): Todo
+        realEstateObject(filter: Filter): [RealEstateObject]!
+        realEstateObjects(limit: Int! = 5, start: Int! = 0): [RealEstateObject]!
     }
 `;
 
-const loadData = (url) => fetch(url).then((res) => res.json());
+// const loadData = (url) => fetch(url).then((res) => res.json());
+
 
 const resolvers = {
     Query: {
-        todos: (obj, { start, limit }) => {
-            const url = new URL(todosUrl);
-            url.searchParams.append('_start', start);
-            url.searchParams.append('_limit', limit);
-            return loadData(url).then((result) => result);
-        },
-        todo: (obj, { id }) => loadData(`${todosUrl}/${id}`)
-            .then((result) => result),
+        // todos: (obj, { start, limit }) => {
+        //     const url = new URL(todosUrl);
+        //     url.searchParams.append('_start', start);
+        //     url.searchParams.append('_limit', limit);
+        //     return loadData(url).then((result) => result);
+        // },
+        // todo: (obj, { id }) => loadData(`${todosUrl}/${id}`)
+        //     .then((result) => result),
+        realEstateObjects: (props) => {
+            console.log('___', props);
+        }
     },
 }
 
